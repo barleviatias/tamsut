e294d22d9aa702fc4037ee848df7c6dfa186a469
barleviatias <80356742+barleviatias@users.noreply.github.com>
2025-06-08 08:44:46 +0000

    updatte

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -22,14 +22,24 @@ export default function Footer() {
 						שלנו דוגל במקצועיות, אמינות ושירות אישי לכל לקוח.
 					</p>
 					<div className="flex justify-center items-center mb-4">
-						<div className="flex flex-col items-center mr-8">
+						<a
+							href="tel:050-7282180"
+							className="flex flex-col items-center mr-8 hover:text-blue-500"
+							aria-label="Call 050-7282180"
+						>
 							<Phone className="w-6 h-6 mb-2" />
 							<span>050-7282180</span>
-						</div>
-						<div className="flex flex-col items-center ml-8 mr-8">
+						</a>
+
+						<a
+							href="mailto:tamsutlaw@gmail.com"
+							className="flex flex-col items-center mx-8 hover:text-blue-500"
+							aria-label="Email tamsutlaw@gmail.com"
+						>
 							<Mail className="w-6 h-6 mb-2" />
 							<span>tamsutlaw@gmail.com</span>
-						</div>
+						</a>
+
 						<div className="flex flex-col items-center ml-8">
 							<MapPin className="w-6 h-6 mb-2" />
 							<span>פריסה ארצית</span>
